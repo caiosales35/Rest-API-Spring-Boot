a3f0d67132cb6d9d6b7c5cb88c3ec3805bac96df
Caio Sales <caio.cesar@liven.tech>
2022-03-26 01:19:03 -0300

    fix: crossOrigin request

--- src/main/java/com/api/rest/resources/ImovelResource.java
+++ src/main/java/com/api/rest/resources/ImovelResource.java
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  *
  * @author caiocesardemorais
  */
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api/v1")
 public class ImovelResource {
